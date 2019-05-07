8011c5cf32ce1ff94eb2cdd8d4d77201635e7124
War10c3 <sankulthakur@gmail.com>
2019-05-07 15:27:40 +0530

    Update assign11.py

--- assign11.py
+++ assign11.py
@@ -16,7 +16,7 @@ emails=''' abc@gmail.com
             xyz@yahoo.com
             jainyz.yahoo.com
             str892@gmail.@com
-            sarthakdj_a@hotmail.com'''
+            dj_a@hotmail.com'''
 pattern= re.compile('[a-zA-Z0-9_.]+@{1}[a-zA-Z0-9]+\.[a-zA-Z0-9]+')
 matches= pattern.findall(emails)
 for i in matches:
